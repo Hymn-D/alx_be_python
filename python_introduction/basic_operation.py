c808361478c79d0cc385acbf37da03954e273283
esther <ogbonnaesther52@gmail.com>
2025-07-13 20:22:17 +0100

    add basic_operations.py script

--- python_introduction/basic_operation.py
+++ python_introduction/basic_operation.py
@@ -1,10 +1,10 @@
-["number1 = 10", "number2 = 5"]
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print("Addition of", number1, "and", number2 "is", sum")
 
-sum_result = number1 + number2
-diff_result = number1 - number2
-product_result = number1 * number2
+difference = number1 - number2
+print("Subtraction of", number1, "and" number2 "is",difference")
 
-print(f"Addition of {number1} and {number2} is {sum_result}")
-print(f"Subtraction of {number1} and {number2} is {diff_result}")
-print(f"Multiplication of {number1} and {number2} is {product_result}")
-
+product = number1 * number2
+print("Multiplication of", number1 "and" number2 "is" product")
