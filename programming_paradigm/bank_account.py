223d84920ecd9c7652b004e0a34dfe39bf9225cf
esther <ogbonnaesther52@gmail.com>
2025-08-08 10:27:15 +0100

    add bank account script

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,5 +13,6 @@ class BankAccount:
         else:
             return False
 
-    def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+def display_balance(self):
+    print(f"Current Balance: ${self.account_balance:.2f}")
+
